ab311ec8cb22d9946ed7a949b6d1b01eed41e303
igorgaidolfi <gandalf.4@outlook.it>
2023-11-24 17:53:35 +0100

    update Edited array_tail\script ES Completato

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -6,4 +6,10 @@ for(let i=0; i<arrElements; i++){
     // 3. RIEMPIRE GLI ELEMENTI DELL'ARRAY CON UN NUMERO RANDOM DA 1 A 100
     array.push(Math.floor(Math.random() * 100)+ 1)
 }
-console.log(array)
+if(arrElements >= 5){
+    const array2 = array.slice(arrElements - 5)
+    console.log(array2)
+}else{
+    alert('Gli elementi presenti sono minori di 5')
+    console.log(array)
+}
